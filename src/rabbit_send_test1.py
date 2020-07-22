3c8fe7727cfd826ebd42c9b6221c7bd9fe95f165
skinn <skinn@HUAN-SOLO>
2020-07-22 15:54:57 +0100

    Modified test message to ressemble EC msg

--- src/rabbit_send_test1.py
+++ src/rabbit_send_test1.py
@@ -7,6 +7,12 @@ channel = connection.channel()
 
 channel.queue_declare(queue='machine_status_1')
 
-channel.basic_publish(exchange='', routing_key='machine_status_1', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+machine_id = 'test_local'
+name = 'spindle_on_off'
+value = 1
+
+message = "time:" + datetime.datetime.now() + "|machineID:" + machine_id + "|" + name + ":" + str(value)
+
+channel.basic_publish(exchange='', routing_key='machine_status_1', body=message)
+print(" [x] Sent:" + message)
 connection.close()
